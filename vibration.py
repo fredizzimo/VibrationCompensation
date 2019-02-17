f584f13f3c1677944d8f1f501682724aa96fd096
Fred Sundvik <fsundvik@gmail.com>
2019-02-17 18:58:30 +0200

    Add layer information

--- vibration.py
+++ vibration.py
@@ -2,6 +2,7 @@ import argparse
 import pandas
 import bokeh
 import bokeh.plotting
+import numpy as np
 
 def read_gcode(f):
     move_commands = []
@@ -13,13 +14,23 @@ def read_gcode(f):
             move_commands.append({arg[0]:float(arg[1:]) for arg in args})
 
     df = pandas.DataFrame.from_records(move_commands, columns=["x", "y", "z", "e", "f"])
-    df.fillna(method="ffill", inplace=True)
-    df.fillna(0.0, inplace=True)
+    all_expect_e = df.columns.difference(["e"])
+    df.iloc[0].fillna(0.0, inplace=True)
+    df[all_expect_e] = df[all_expect_e].fillna(method="ffill")
+    df["e"].fillna(0.0, inplace=True)
+
+    # Calculate layer numbers, by excluding non-print moves (which could include z-hop)
+    # Non printing moves are moves with an extrude of zero
+    df["layer"] = df[df.e !=0.0].groupby("z").ngroup()
+    df.loc[0:1, "layer"].fillna(0.0, inplace=True)
+    df["layer"].fillna(method="ffill", inplace=True)
+    df["layer"] = df["layer"].astype(int)
+    print(df[:100])
     return df
 
 def plot(df):
     bokeh.plotting.output_file("output.html")
-    p = bokeh.plotting.figure(plot_width=400, plot_height=400)
+    p = bokeh.plotting.figure(plot_width=400, plot_height=400, match_aspect=True)
 
     # add a line renderer
     p.line(df["x"], df["y"], line_width=0.25)
@@ -30,7 +41,7 @@ def main():
     parser.add_argument("input", type=argparse.FileType("r"))
     args = parser.parse_args()
     df = read_gcode(args.input)
-    plot(df[:500])
+    plot(df[df.layer == 1])
 
 
 if __name__ == "__main__":
