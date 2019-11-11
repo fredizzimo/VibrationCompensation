aaf0b96723f782afd739c43801672ab3a4f5e80f
fred.sundvik <fsundvik@gmail.com>
2019-11-11 20:56:39 +0200

    Try to cleanup the signal with gradient method

--- notebooks/analyse_accel.py
+++ notebooks/analyse_accel.py
@@ -18,6 +18,8 @@ import numpy as np
 import numpy.linalg as npl
 import plotly.graph_objects as go
 import pywt
+import scipy.fftpack as fftpack
+import scipy.signal as signal
 
 # %%
 res = np.loadtxt("../data/2019-04-0619.56.55.csv.txt", delimiter=",", skiprows=1)
@@ -74,19 +76,95 @@ fig.show()
 
 
 # %%
-def generate_equally_sampled_signal(time, signal):
+def plot_fft(fft):
+    fig = go.Figure()
+    freqs = fftpack.fftfreq(fft.shape[0], 1.0/1000)
+    freqs = fftpack.fftshift(freqs)
+    fft = fftpack.fftshift(fft)
+    fig.add_trace(go.Scatter(x=freqs, y=np.abs(fft)))
+    fig.show()
+
+
+# %%
+def cleanup_signal(time, signal):
+    
+    delta = np.max(np.abs(signal))
+    
     time = np.array(np.round(time * 1000), dtype=np.int)
     nogap = np.arange(time[0], time[-1] + 1, 1)
     existing_times = np.isin(nogap, time, assume_unique=True)
     nonexisting_times = ~existing_times
+    num_nonexisting = np.sum(nonexisting_times)
     full_signal = np.zeros(nogap.shape[0])
     full_signal[existing_times] = signal
-    signal = full_signal
-    time = nogap 
-    coeffs = pywt.wavedec(signal, 'db1', level=8, mode="zero")
+    
     fig = go.Figure()
+    fig.add_trace(go.Scatter(x=time, y=signal))
     fig.add_trace(go.Scatter(x=nogap, y=full_signal))
-    for c in coeffs[1:]:
-        fig.add_trace(go.Scatter(x=nogap, y=c))
     fig.show()
-generate_equally_sampled_signal(time_fa, ay_fa)
+    if False:
+        #signal = full_signal
+        #time = nogap 
+        N = full_signal.shape[0]
+        y = full_signal
+
+        #D = np.zeros((N, N))
+        #np.fill_diagonal(D, 1)
+        #D = fftpack.fft(D, axis=1)
+        #ones = np.ones(N)
+
+        test = np.array([[1, 2],[3, 4]])
+        print(np.sum(test, axis=1))
+        print(test[0])
+
+        for _ in range(5):
+            #scaled_delta = delta*D[nonexisting_times]
+            #d2 = delta*np.ones(N)
+            diagonal_delta = np.zeros((N, N))
+            np.fill_diagonal(diagonal_delta, delta)
+            for _ in range(20):
+                filled_y = np.tile(y, (N, 1))
+                #Y = fftpack.fft(y)
+                f1=filled_y[nonexisting_times] + diagonal_delta[nonexisting_times]
+                f2=filled_y[nonexisting_times] - diagonal_delta[nonexisting_times]
+                F1 = fftpack.fft(f1, axis=1)
+                F2 = fftpack.fft(f2, axis=1)
+                #g = np.sum(np.abs(Y + scaled_delta), axis=1)
+                #g -= np.sum(np.abs(Y - scaled_delta), axis=1)
+                g = np.sum(np.abs(F1), axis=1)
+                g -= np.sum(np.abs(F2), axis=1)
+                g /= N
+                y[nonexisting_times] -= g
+            delta = delta / np.sqrt(10)
+            #delta /= 2.0
+
+    N = full_signal.shape[0]
+    y = full_signal
+    
+    D = np.zeros((N, N))
+    np.fill_diagonal(D, 1)
+    D = fftpack.fft(D, axis=1)
+    
+    for _ in range(20):
+        scaled_delta = delta*D[nonexisting_times]
+        for _ in range(5):
+            Y = fftpack.fft(y)
+            g = np.sum(np.abs(Y + scaled_delta), axis=1)
+            g -= np.sum(np.abs(Y - scaled_delta), axis=1)
+            g /= N
+            y[nonexisting_times] -= g
+        delta = delta / np.sqrt(10)
+        #delta /= 2.0
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=time, y=signal))
+    fig.add_trace(go.Scatter(x=nogap, y=y))
+    fig.show()
+    #plot_fft(Y)
+    
+    
+cleanup_signal(time_fa, ay_fa - 0.4)
+
+# %%
+
+# %%
